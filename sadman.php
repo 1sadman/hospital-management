5e5326606ec8aa7d7cdd877498025cc72b78b903
1sadman <110235005+1sadman@users.noreply.github.com>
2022-10-01 03:34:44 +0600

    Add files via upload

---
+++ sadman.php
@@ -0,0 +1,515 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospital Project  </title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="sadman.css">
+
+    <!-- custom css file link  -->
+   
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo"> <i class="fas fa-heartbeat"></i> City Hospital. </a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#services">services</a>
+        <a href="#about">about</a>
+        <a href="#doctors">doctors</a>
+        <a href="#appointment">appointment</a>
+        <a href="#review">review</a>
+        <a href="#blogs">blogs</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="image">
+        <img src="image/home-img.svg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>stay safe, stay healthy</h3>
+        <p>City Hospital Ltd started on 19-02-1999 as 20 bed Specialized Burn & Plastic Surgery Hospital at 69/I/1, Panthapath, Dhanmondi, Dhaka-1205, then shifted to Lalmatia, Dhaka from 15th July, 2006 as 100 beded general hospital including 24 bed Burn & Plastic Surgery (8 High Dependency Unit beds and 16 general beds).</p>
+        <a href="#" class="btn"> contact us <span class="fas fa-chevron-right"></span> </a>
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- icons section starts  -->
+
+<section class="icons-container">
+
+    <div class="icons">
+        <i class="fas fa-user-md"></i>
+        <h3>140+</h3>
+        <p>doctors at work</p>
+    </div>
+
+    <div class="icons">
+        <i class="fas fa-users"></i>
+        <h3>1040+</h3>
+        <p>satisfied patients</p>
+    </div>
+
+    <div class="icons">
+        <i class="fas fa-procedures"></i>
+        <h3>500+</h3>
+        <p>bed facility</p>
+    </div>
+
+    <div class="icons">
+        <i class="fas fa-hospital"></i>
+        <h3>10+</h3>
+        <p>available hospitals</p>
+    </div>
+
+</section>
+
+<!-- icons section ends -->
+
+<!-- services section starts  -->
+
+<section class="services" id="services">
+
+    <h1 class="heading"> our <span>services</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <i class="fas fa-notes-medical"></i>
+            <h3>free checkups</h3>
+            <p>We Provide Free Checkup To Old People And Pooe People</p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-ambulance"></i>
+            <h3>24/7 ambulance</h3>
+            <p>Call On These Numbers For Ambulance <b> 01928287367 <br> 9873828</b></p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-user-md"></i>
+            <h3>expert doctors</h3>
+            <p>We Provide expart Doctors For Various Disease </p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-pills"></i>
+            <h3>medicines</h3>
+            <p>We have special medicin doctor. We provide all kinds of ncessary medicin</p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-procedures"></i>
+            <h3>bed facility</h3>
+            <p>We H=have more then 500 beds and 250 cabin</p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-heartbeat"></i>
+            <h3>total care</h3>
+            <p>We have total 10 hospitals in bangladesh</p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <h1 class="heading"> <span>about</span> us </h1>
+
+    <div class="row">
+
+        <div class="image">
+            <img src="image/about-img.svg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>we take care of your healthy life</h3>
+            <p>City Hospital Specialized Hospital, a concern of City Hospital Group is the only multi-disciplinary super-specialty tertiary care hospital in Bangladesh, confidently providing comprehensive health care with the latest medical, surgical and diagnostic facilities. These services are provided by expert medical professionals, skilled nurses and technologists using state-of-the-art technology.
+
+City Hospital Specialized Hospitalhas all the characteristics of a world-class hospital with wide range of services and specialists, equipments and technology, ambience and service quality. The hospital is a showcase of synergy of medical technology and advances in IT through paperless medical records. The skilled nurses, technologists and administrators of City Hospital Specialized Hospital, aided by state-of-the-art equipments, provide a congenial infrastructure for the medical professionals in providing healthcare of international standards.</p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- doctors section starts  -->
+
+<section class="doctors" id="doctors">
+
+    <h1 class="heading"> our <span>doctors</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="image/one.jpg" alt="">
+            <h3>Dr. Md. Mahmudul Haque</h3>
+            <span>MBBS, FCPS (ENT)
+Ear, Nose, Throat Specialist & Head Neck Surgeon <br> mahamudul@gmail.com
+</span>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/two.jpg" alt="">
+            <h3>Dr. Mohammad Kamruzzaman</h3>
+            <span>MBBS, FCPS (ENT), MS (ENT)
+Ear, Nose, Throat Specialist & Head Neck Surgeon <br> kamrul@gmail.com
+</span>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/three.jpg" alt="">
+            <h3>Prof. Dr. Abdullah Al Mahboob</h3>
+            <span>MBBS, FCPS (CHILD)
+Newborn, Child & Adolescent Diseases Specialist <br> abdullah@gmail.com
+</span>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/four.jpg" alt="">
+            <h3>Dr. AKM Mamunur Rashid</h3>
+            <span>MBBS, DCH, MD (CHILD), FRCP (UK)
+Newborn, Adolescent & Child Diseases Specialis <br> tmamur @gmail.com</span>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/five.jpg" alt="">
+            <h3>Dr. S. M. Mainul Haque</h3>
+            <span>
+MBBS, BCS (Health), DTCD, MD (CHEST)
+Chest Diseases, Asthma & Respiratory Medicine Specialist mainul@gmail.com</span>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/six.jpg" alt="">
+            <h3>Prof. Dr. Khosrul Alam</h3>
+            <span>
+MBBS, MCPS (Medicine), MD (Chest Diseases)
+Chest Diseases, Asthma & Respiratory Medicine Specialist
+Mail:Khosrul@gmail.com</span>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- doctors section ends -->
+
+<!-- booking section starts   -->
+
+<section class="book" id="book">
+
+    <h1 class="heading"> <span>book</span> now </h1>    
+
+    <div class="row">
+
+        <div class="image">
+            <img src="image/book-img.svg" alt="">
+        </div>
+
+        <form action="" method ="POST">
+            <h3>book appointment</h3>
+            <input type="text" name= "text" placeholder="your name" class="box">
+            <input type="number" name= "number" placeholder="your number" class="box">
+
+            <input type="email" name= "email" placeholder="your email" class="box">
+            <input type="date" name="date" class="box">
+             <input type="text" name= "text" placeholder="your disase" class="box">
+
+            <input type="submit" name="submit" value="book now" class="btn">
+        </form>
+
+    </div>
+
+</section>
+<?php
+
+
+if(isset($_POST['submit'])){
+
+ $text = $_POST['text'];
+ $number = $_POST['number'];
+ $email = $_POST['email'];
+ $date = $_POST['date'];
+  $text = $_POST['text'];
+
+
+ $connect = new mysqli("localhost","root","","hospital");
+
+ $sql = ("INSERT INTO hos(name, number, email, date,disase) VALUES ('$text','$number','$email','$date','$date')");
+
+ if(mysqli_query($connect,$sql))
+{
+    echo "Appointment Successfull....";
+}
+else
+{
+    echo "Something went wrong. Try again.....";
+}
+}
+?>
+
+<!-- booking section ends -->
+
+<!-- review section starts  -->
+
+<section class="review" id="review">
+    
+    <h1 class="heading"> client's <span>review</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="image/eleven.jpg" alt="">
+            <h3>Monir Zaman</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <p class="text">Recently visited health check up department of city Hospital for a whole body check up. The entire process from check in to the end was seamless. The staff at the health check department was courteous, helpful and very attentive. I would highly recommend this hospital to anyone looking for something similar services.</p>
+        </div>
+
+        <div class="box">
+            <img src="image/twelve.jpg" alt="">
+            <h3>Nazia Akter</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <p class="text">Dr. AKM Mamunur Rashid: Dr. AKM Mamunur Rashid last month with severe cough and sputum for my father Dr Varaprasad. His way of handling patients is excellent and so soothing that the patient feels immensely confident about recovery. Thanks to Dr. Vijay, within 2 weeks, he is completely normal.</p>
+        </div>
+
+        <div class="box">
+            <img src="image/ten.jpg" alt="">
+            <h3>Sayed Hossain</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <p class="text">In City hospital Dr.Radha Shah at reception people Mr.Venkat helping to patients excelent way of speaking Guidense.. I felt very Happy with this people.. I strongly recommend to my Friends n Relatives to this City Hospital!</p>
+        </div>
+
+
+
+
+    </div>
+
+
+</section>
+
+<!-- review section ends -->
+
+<!-- blogs section starts  -->
+
+<section class="blogs" id="blogs">
+
+    <h1 class="heading"> our <span>blogs</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="image/blog-1.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i> 1st may, 2021 </a>
+                    <a href="#"> <i class="fas fa-user"></i> by admin </a>
+                </div>
+                <h3>Fever treatment: Quick guide to treating a fever</h3>
+                <p>A fever is a common sign of illness, but that's not necessarily a bad thing. In fact, fevers seem to play a key role in fighting infections. So should you treat a fever or let the fever run its course? Here's help making the call. These recommendations are for otherwise healthy</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="image/blog-2.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i> 1st may, 2021 </a>
+                    <a href="#"> <i class="fas fa-user"></i> by admin </a>
+                </div>
+                <h3>Blood in the stool</h3>
+                <p>  Blood in the stool can be frightening, whether you discover it while wiping after a bowel movement or from a test ordered by your health care provider. While blood in stool can signal a serious problem, it doesn't always. Here's what you need to know about the possible causes of bloody stools and what you</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="image/blog-3.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i> 1st may, 2021 </a>
+                    <a href="#"> <i class="fas fa-user"></i> by admin </a>
+                </div>
+                <h3>Cause of Chest Pain</h3>
+                <p>Chest pain. The first thing you may think of is the heart attack. Certainly, chest pain is not something to ignore. But you should know that it has many possible causes. In fact, as much as a quarter of the U.S. population experiences chest pain that is not related to the heart.</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- blogs section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#"> <i class="fas fa-chevron-right"></i> home </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> services </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> about </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> doctors </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> book </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> review </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> blogs </a>
+        </div>
+
+        <div class="box">
+            <h3>our services</h3>
+            <a href="#"> <i class="fas fa-chevron-right"></i> dental care </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> message therapy </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> cardioloty </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> diagnosis </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> ambulance service </a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> 01775092473 </a>
+            <a href="#"> <i class="fas fa-phone"></i> 01737870690 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> sadmanfahim826@gmail.com </a>
+            <a href="#"> <i class="fas fa-envelope"></i> fahimsadman826@gmail.com </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Khulna,Bangladesh </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+           
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+            
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>Sadman Hossain</span> | all rights reserved </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
